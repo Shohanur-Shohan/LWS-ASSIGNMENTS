f1debf805a8dc34d9d5ad4e811670a3f307d82d0
Shohanur-Shohan <shohanurshohan734@gmail.com>
2024-10-13 23:48:06 +0600

    onclick func added

--- think-in-a-redux-way-1.13/html/index.js
+++ think-in-a-redux-way-1.13/html/index.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const INCREMENT = document.querySelector(".lws-increment")
+const DECREMENT = document.querySelector(".lws-decrement")
+const IncrementForm = document.querySelector(".incrementForm")
+const DecrementForm = document.querySelector(".decrementForm")
+const DeleteItem = document.querySelector(".lws-delete")
+const AddMatch = document.querySelector(".lws-addMatch")
+const PointResult = document.querySelector(".lws-singleResult")
+const Reset = document.querySelector(".lws-reset")
+
+const increment = () => {
+  const point = PointResult
+  console.log(point.innerHTML)
+}
+
+AddMatch.addEventListener("click", () => {
+  console.log("clicked")
+  increment()
+})
